ba228b8fe6ec20578a6f2814aa178781705e7b7c
yongheng798 <1351817260@qq.com>
2020-06-12 11:24:21 +0800

    add: new hello world

---
+++ src/components/demo/HelloWorld.vue
@@ -0,0 +1,24 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-06-12 11:20:33
+ * @LastEditTime: 2020-06-12 11:21:36
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \mi\src\components\demo\HelloWorld.vue
+-->
+<template>
+  <div class="containner">
+    <h3>{{msg}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "Hello world"
+    };
+  }
+};
+</script>
